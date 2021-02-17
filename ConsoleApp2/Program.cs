ee419176fe2601c1ae487f5d4f40ef3cea43242e
Onur Öner <onuroner_@hotmail.com>
2021-02-17 21:31:02 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYUapmisMi = true;
+
+            if (sistemeGirisYUapmisMi==true)
+            {
+                Console.WriteLine("Ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            double DolarDun = 7.45;
+            double DolarBugun = 7.45;
+
+            if (DolarBugun > DolarDun)
+            {
+                Console.WriteLine("Dolar Artmış");
+            }
+            else if (DolarBugun<DolarDun) 
+            {
+                Console.WriteLine("Dolar Azalmış");
+            }
+            else
+            {
+                Console.WriteLine("Dolar Sabit");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(ogrenciSayisi);
+            Console.WriteLine(faizOrani);
+            Console.ReadLine();
+
+        }
+        
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[]
+            {
+                "Yazılım Geliştirici Kampı",
+                "Programlamaya başlamak için temel kurs",
+                "Java",
+                "Kırmançi",
+                "Geroiç"
+            };
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+            Console.WriteLine("********");
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
