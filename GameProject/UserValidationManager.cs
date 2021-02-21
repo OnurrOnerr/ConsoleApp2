220418182e2fd8316840320343f8a23c734ab147
Onur Öner <onuroner_@hotmail.com>
2021-02-21 13:59:39 +0300

    21 Subat Ders Sonu

---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1985 && gamer.FirstName == "Engin" && gamer.LastName == "Demiroğ")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
